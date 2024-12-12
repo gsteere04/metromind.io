33da7743873e22a53a59f8a91f3d40a45022e9bd
Grant <gsteere4717@xmail.dixietech.edu>
2024-12-12 02:46:39 -0700

    fix deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import SimulationPage from '../pages/SimulationPage';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter basename="/metromind.io">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/simulation" element={<SimulationPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
